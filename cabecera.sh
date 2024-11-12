f552b3e81a384e5e944cb61639de7ae3fdfe327c
Miguel Fernandez <fdezguerrerom@gmail.com>
2024-11-12 16:12:43 +0100

    Enlazado w3.css

--- cabecera.sh
+++ cabecera.sh
@@ -5,6 +5,7 @@ cat << EOF
         <html>
             <head>
                 <meta charset='utf-8'>
+                <link rel="stylesheet" href="w3.css" />
                 <title>Página de inicio</title>
             </head>
             <body>
